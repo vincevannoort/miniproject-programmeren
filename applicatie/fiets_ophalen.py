e380cff70fe5c4c33dc5e5ce40b3a71b8f307f07
DiederikSchouten <diederik.schouten@student.hu.nl>
2016-10-26 14:46:17 +0200

    Basis Fiets Ophalen functie (Met Jeroen)
    Werkende functie voor het ophalen van de fiets

--- applicatie/fiets_ophalen.py
+++ applicatie/fiets_ophalen.py
@@ -1,2 +1,26 @@
-def fiets_ophalen():
-  pass
+import sqlite3
+import datetime
+
+def fiets_ophalen(unieknummer):
+    conn = sqlite3.connect('data/fietsenstalling.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS stallingen(id INTEGER PRIMARY KEY AUTOINCREMENT, unieknummer INTEGER NOT NULL, startdatum DATETIME NOT NULL, einddatum DATETIME)')
+
+#Gebruiker voert nummer in
+    while unieknummer == '':
+        invoer = int(input('Geef uw unieke nummer: '))
+
+        #checkt of de fiets nu gestald is
+        c.execute('SELECT * FROM stallingen WHERE unieknummer = {}'.format(invoer))
+        resultaat = c.fetchone()
+
+        if resultaat:
+            end_time = datetime.datetime.now()
+            c.execute("UPDATE stallingen SET einddatum = '{}' WHERE unieknummer = '{}'".format(end_time, invoer))
+            conn.commit()
+            conn.close()
+            print('Uw fiets is opgehaald, doei!')
+        else:
+            print('Dit is is geen geldig nummer')
+
+fiets_ophalen('')
