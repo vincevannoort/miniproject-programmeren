6753cdc224b5c98a68823dc1a38ee8f44829a2ec
Vince van Noort <vince.vannoort@student.hu.nl>
2016-10-26 14:14:07 +0200

    Basis fiets stallen functie (met Diederik & Jeroen)

--- applicatie/fiets_stallen.py
+++ applicatie/fiets_stallen.py
@@ -1,2 +1,30 @@
-def stallen():
-  pass
+import sqlite3
+import datetime
+
+def stallen(unieknummer):
+
+  # probeer connectie met database te leggen
+  conn = sqlite3.connect('data/fietsenstalling.db')
+  c = conn.cursor()
+  c.execute('CREATE TABLE IF NOT EXISTS stallingen(id INTEGER PRIMARY KEY AUTOINCREMENT, unieknummer INTEGER NOT NULL, startdatum DATETIME NOT NULL, einddatum DATETIME)')
+
+  # als er geen unieknummer is gemaakt
+  while unieknummer == '':
+    ingevoerd_unieknummer = int(input('Voer je unieke nummer in: '))
+
+    # check if unieknummer is in de database
+    c.execute('SELECT * FROM registratie WHERE unieknummer = {}'.format(ingevoerd_unieknummer))
+    resultaat = c.fetchone()
+
+    # als er een resultaat is, zet dan het unieke nummer naar het 2e veld in de tuple om dat while loop te stoppen
+    if resultaat:
+      unieknummer = resultaat[1]
+
+  # sla de stalgegevens op in de SQLite database
+  current_time = datetime.datetime.now()
+  print(current_time)
+
+  c.execute("INSERT INTO stallingen (unieknummer, startdatum) VALUES ('{}', '{}')".format(unieknummer, current_time))
+  conn.commit()
+  conn.close()
+
