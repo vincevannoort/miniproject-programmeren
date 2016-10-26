8cb2e6b27ca9c2d3ff3e6db92dc22e960897e024
Vince van Noort <vince.vannoort@student.hu.nl>
2016-10-26 19:26:21 +0200

    Registratie functie & Headers

--- applicatie/applicatie_fietsenstalling.py
+++ applicatie/applicatie_fietsenstalling.py
@@ -37,13 +37,17 @@ class FietsenStallingApp(Tk):
       frame = self.frames[page_name]
       frame.tkraise()
 
+  # registratie functie aanroepen met inputs
+  def registratie_doorvoeren(event, naam, tel, mail):
+    registratie(naam, tel, mail)
+
 class PageOverzicht(Frame):
   def __init__(self, parent, controller):
     Frame.__init__(self, parent)
     self.controller = controller
 
     # Header
-    header = Frame(self, height=125, width=1280, bg='white')
+    header = Frame(self, width=1280, bg='white')
     header.pack(side=TOP, fill=X)
 
     # Body
@@ -53,6 +57,7 @@ class PageOverzicht(Frame):
     # Fonts
     font_header = font.Font(family='Open Sans', size=32, weight='normal')
     font_body = font.Font(family='Open Sans', size=16, weight='normal')
+    Label(header, text='NS | Fietsenstalling', font=font_header, bg='white', fg='#003082', anchor='w', padx=80).pack(fill='both', pady=30)
 
     # 1. Registreren
     registreren = Frame(body, height=225, width=520, bg='#E6B517')
@@ -93,7 +98,7 @@ class PageRegistreren(Frame):
     self.controller = controller
 
     # Header
-    header = Frame(self, height=125, width=1280, bg='white')
+    header = Frame(self, width=1280, bg='white')
     header.pack(side=TOP, fill=X)
 
     # Body
@@ -103,6 +108,7 @@ class PageRegistreren(Frame):
     # Fonts
     font_header = font.Font(family='Open Sans', size=32, weight='normal')
     font_body = font.Font(family='Open Sans', size=16, weight='normal')
+    Label(header, text='NS | Registreren', font=font_header, bg='white', fg='#003082', anchor='w', padx=80).pack(fill='both', pady=30)
 
     # 1. Registreren
     registreren = Frame(body, height=526, width=1120, bg='#E6B517')
@@ -111,21 +117,21 @@ class PageRegistreren(Frame):
     Label(registreren, text='Registreren', font=font_header, bg='#E6B517', fg='#003082', anchor='w', padx=30, pady=15).pack(fill='both')
     Label(registreren, text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nIn urna tellus, egestas vel leo quis, faucibus porta orci.', padx=30, font=font_body, bg='#E6B517', fg='#392D05', anchor='w', justify=LEFT).pack(fill='both')
 
-
+    # Registratie velden
     entryNaam = Entry(registreren, font=font_body, bg='white', justify=LEFT)
     entryNaam.insert(0, 'Naam:')
     entryNaam.pack(anchor='w', padx=30, pady=(30,10))
-    entryEmail = Entry(registreren, font=font_body, bg='white', justify=LEFT)
-    entryEmail.insert(0, 'Email:')
-    entryEmail.pack(anchor='w', padx=30, pady=10)
     entryTel = Entry(registreren, font=font_body, bg='white', justify=LEFT)
     entryTel.insert(0, 'Telefoonnummer:')
-    entryTel.pack(anchor='w', padx=30, pady=(10,30))
+    entryTel.pack(anchor='w', padx=30, pady=10)
+    entryEmail = Entry(registreren, font=font_body, bg='white', justify=LEFT)
+    entryEmail.insert(0, 'Email:')
+    entryEmail.pack(anchor='w', padx=30, pady=(10,30))
 
-    Button(registreren, text='Terug  >', padx=30, pady=10, font=font_body, bg='#0079D3', fg='white', anchor='w', justify=LEFT, command=lambda: controller.show_frame('PageOverzicht')).pack(anchor='w', padx=30, pady=10)
-
-
-
+    # Als de button wordt ingedrukt, wordt de functie registratie_doorvoeren binnen de controller aangeroepen die de parameters van de input velden meekrijgt
+    Button(registreren, text='registreer  >', padx=30, pady=10, font=font_body, bg='#0079D3', fg='white', anchor='w', justify=LEFT, 
+        command=lambda: registratie(entryNaam.get(), entryTel.get(), entryEmail.get())).pack(side='left', anchor='w', padx=(30, 10))
+    Button(registreren, text='terug  >', padx=30, pady=10, font=font_body, bg='#0079D3', fg='white', anchor='w', justify=LEFT, command=lambda: controller.show_frame('PageOverzicht')).pack(side='left', anchor='w', padx=(10, 30))
 
 class PageStallen(Frame):
   def __init__(self, parent, controller):
@@ -137,17 +143,18 @@ class PageStallen(Frame):
     header.pack(side=TOP, fill=X)
 
     # Body
-    body = Frame(self, height=675, bg='#FFC400', padx=80, pady=45)
+    body = Frame(self, bg='#FFC400', padx=80, pady=45)
     body.pack(side=TOP, fill=X)
 
     # Fonts
     font_header = font.Font(family='Open Sans', size=32, weight='normal')
     font_body = font.Font(family='Open Sans', size=16, weight='normal')
+    Label(header, text='NS | Stallen', font=font_header, bg='white', fg='#003082', anchor='w', padx=80).pack(fill='both', pady=30)
 
     # 2. Fiets stallen
     stallen = Frame(body, height=526, width=1120, bg='#E6B517')
     stallen.pack_propagate(0)
-    stallen.grid(row=1, column=2, padx=(40, 0), pady=(35,40))
+    stallen.grid(row=1, column=1, padx=(0, 40), pady=(35,40))
     Label(stallen, text='Stallen', font=font_header, bg='#E6B517', fg='#003082', anchor='w', padx=30, pady=15).pack(fill='both')
     Label(stallen, text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nIn urna tellus, egestas vel leo quis, faucibus porta orci.', padx=30, font=font_body, bg='#E6B517', fg='#392D05', anchor='w', justify=LEFT).pack(fill='both')
     Button(stallen, text='Terug  >', padx=30, pady=10, font=font_body, bg='#0079D3', fg='white', anchor='w', justify=LEFT, command=lambda: controller.show_frame('PageOverzicht')).pack(side='left', padx=30)
@@ -158,7 +165,7 @@ class PageInformatie(Frame):
     self.controller = controller
 
     # Header
-    header = Frame(self, height=125, width=1280, bg='white')
+    header = Frame(self, width=1280, bg='white')
     header.pack(side=TOP, fill=X)
 
     # Body
@@ -168,11 +175,12 @@ class PageInformatie(Frame):
     # Fonts
     font_header = font.Font(family='Open Sans', size=32, weight='normal')
     font_body = font.Font(family='Open Sans', size=16, weight='normal')
+    Label(header, text='NS | Informatie ophalen', font=font_header, bg='white', fg='#003082', anchor='w', padx=80).pack(fill='both', pady=30)
 
     # 3. Informatie ophalen
     informatie = Frame(body, height=526, width=1120, bg='#E6B517')
     informatie.pack_propagate(0)
-    informatie.grid(row=2, column=1, padx=(0, 40), pady=(40,35))
+    informatie.grid(row=1, column=1, padx=(0, 40), pady=(35,40))
     Label(informatie, text='Informatie ophalen', font=font_header, bg='#E6B517', fg='#003082', anchor='w', padx=30, pady=15).pack(fill='both')
     Label(informatie, text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nIn urna tellus, egestas vel leo quis, faucibus porta orci.', padx=30, font=font_body, bg='#E6B517', fg='#392D05', anchor='w', justify=LEFT).pack(fill='both')
     Button(informatie, text='Terug  >', padx=30, pady=10, font=font_body, bg='#0079D3', fg='white', anchor='w', justify=LEFT, command=lambda: controller.show_frame('PageOverzicht')).pack(side='left', padx=30)
@@ -183,7 +191,7 @@ class PageOphalen(Frame):
     self.controller = controller
 
     # Header
-    header = Frame(self, height=125, width=1280, bg='white')
+    header = Frame(self, width=1280, bg='white')
     header.pack(side=TOP, fill=X)
 
     # Body
@@ -193,11 +201,12 @@ class PageOphalen(Frame):
     # Fonts
     font_header = font.Font(family='Open Sans', size=32, weight='normal')
     font_body = font.Font(family='Open Sans', size=16, weight='normal')
+    Label(header, text='NS | Fiets ophalen', font=font_header, bg='white', fg='#003082', anchor='w', padx=80).pack(fill='both', pady=30)
 
     # 4. Fiets ophalen
     ophalen = Frame(body, height=526, width=1120, bg='#E6B517')
     ophalen.pack_propagate(0)
-    ophalen.grid(row=2, column=2, padx=(40, 0), pady=(40,35))
+    ophalen.grid(row=1, column=1, padx=(0, 40), pady=(35,40))
     Label(ophalen, text='Fiets ophalen', font=font_header, bg='#E6B517', fg='#003082', anchor='w', padx=30, pady=15).pack(fill='both')
     Label(ophalen, text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nIn urna tellus, egestas vel leo quis, faucibus porta orci.', padx=30, font=font_body, bg='#E6B517', fg='#392D05', anchor='w', justify=LEFT).pack(fill='both')
     Button(ophalen, text='Terug  >', padx=30, pady=10, font=font_body, bg='#0079D3', fg='white', anchor='w', justify=LEFT, command=lambda: controller.show_frame('PageOverzicht')).pack(side='left', padx=30)
--- applicatie/registratie.py
+++ applicatie/registratie.py
@@ -1,29 +1,29 @@
 import sqlite3
 import random
+from tkinter import messagebox
 
-def registratie():
+def registratie(naam, tel, mail):
   'Registreert een gebruiker in de database'
 
-  # probeer connectie met database te leggen
+  # Probeer connectie met database te leggen
   conn = sqlite3.connect('data/fietsenstalling.db')
   c = conn.cursor()
   c.execute('CREATE TABLE IF NOT EXISTS registratie(id INTEGER PRIMARY KEY AUTOINCREMENT, unieknummer INTEGER NOT NULL, naam TEXT NOT NULL, tel TEXT NOT NULL, email TEXT NOT NULL)')
 
-  # vraag gegevens van de gebruiker
-  gegevensingevuld = False
-  while gegevensingevuld == False:
-    naam = input('Vul je naam in: ')
-    tel = input('Vul je telefoonnummer in: ')
-    mail = input('Vul je e-mail in: ')
-
-    # kijk of alle velden zijn ingevuld
-    if naam != '' and tel != '' and mail != '':
-      gegevensingevuld = True
-
-  # genereer automatisch nummer
+  # Genereer automatisch nummer
   unieknummer = random.randrange(100000, 1000000)
 
-  # sla gegevens op in de database met een uniek nummer en toon deze aan de gebruiker
-  c.execute("INSERT INTO registratie (unieknummer, naam, tel, email) VALUES ('{}', '{}', '{}', '{}')".format(unieknummer, naam, tel, mail))
-  conn.commit()
-  conn.close()
+  # Check of het email adres al bestaat
+  c.execute("SELECT * FROM registratie WHERE email = '{}'".format(mail))
+  emailbestaat = c.fetchone()
+
+  # Sla gegevens op in de database met een uniek nummer en toon deze aan de gebruiker
+  if naam != '' and tel != '' and mail != '' and emailbestaat == None:
+    c.execute("INSERT INTO registratie (unieknummer, naam, tel, email) VALUES ('{}', '{}', '{}', '{}')".format(unieknummer, naam, tel, mail))
+    conn.commit()
+    conn.close()
+    messagebox.showinfo('voltooid' , 'Uw registratie is voltooid! Uw unieke nummer is: {}, bewaar deze goed.'.format(unieknummer))
+  elif emailbestaat != None:
+    messagebox.showinfo('error' , 'Er bestaat el een gebruiker met dit emailadres.')
+  else:
+    messagebox.showinfo('error' , 'Alle velden dienen ingevuld te worden.')
