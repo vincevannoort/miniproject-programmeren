fe3ee730aa99dff9f69dedbd8aac92c6feb2db38
DiederikSchouten <diederik.schouten@student.hu.nl>
2016-10-26 17:15:27 +0200

    Basis functie info_opvragen
    De functie voor het geven van algemene en persoonlijke info

--- applicatie/info_opvragen.py
+++ applicatie/info_opvragen.py
@@ -1,2 +1,25 @@
+import sqlite3
+import datetime
+
 def info_opvragen():
-  pass
+
+  plaatsen_vrij = 1000;
+  conn = sqlite3.connect('data/fietsenstalling.db')
+  c = conn.cursor()
+  c.execute('CREATE TABLE IF NOT EXISTS stallingen(id INTEGER PRIMARY KEY AUTOINCREMENT, unieknummer INTEGER NOT NULL, startdatum DATETIME NOT NULL, einddatum DATETIME)')
+
+  c.execute('SELECT * FROM stallingen WHERE einddatum NOT NULL ORDER BY id')
+  resultaat = c.fetchall()
+  vrije_plaatsen = plaatsen_vrij - len(resultaat)
+  print('Er zijn momenteel {} plaatsen vrij'.format(vrije_plaatsen))
+
+def persoonlijke_info(unieknummer):
+     conn = sqlite3.connect('data/fietsenstalling.db')
+     c = conn.cursor()
+     c.execute('SELECT naam, tel, email FROM registratie WHERE unieknummer = {}'.format(unieknummer))
+     uitdraai = c.fetchone()
+     print('Hallo, {}'.format(uitdraai[0]))
+     print('Uw 06-nummer is {}'.format(uitdraai[1]))
+     print('Uw email adres is {}'.format(uitdraai[2]))
+
+info_opvragen()
